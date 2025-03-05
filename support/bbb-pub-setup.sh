aa6efa177afe1d8408d09a2f5c74f508454c1bcf
SASIDHAR <74709038+sasidhar-serv@users.noreply.github.com>
2025-03-05 17:43:36 +0530

    Update bbb-pub-setup.sh

--- support/bbb-pub-setup.sh
+++ support/bbb-pub-setup.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+
+dns=$1
+email=$2
+
+echo 'Setting up bbb for :'
+echo $1
+echo $2
+wget -qO- https://raw.githubusercontent.com/bigbluebutton/bbb-install/v3.0.x-release/bbb-install.sh | bash -s -- -w -v focal-270 -s $1 -e $2 -g -w
+
+git clone https://github.com/sasidhar-serv/bbb-install.git
+cd bbb-install
+cd support/
+chmod +x support.sh
+bash support.sh
+echo 'All done'
