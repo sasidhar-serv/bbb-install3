bcaa1a734d5ae446eb1c6ed13257ff33df911f22
SASIDHAR <74709038+sasidhar-serv@users.noreply.github.com>
2025-04-26 11:24:08 +0530

    Update bbb-pub-setup.sh

--- support/bbb-pub-setup.sh
+++ support/bbb-pub-setup.sh
@@ -9,7 +9,7 @@ echo $1
 echo $2
 wget -qO- https://raw.githubusercontent.com/bigbluebutton/bbb-install/v3.0.x-release/bbb-install.sh | bash -s -- -w -v jammy-300 -s $1 -e $2 -g
 
-git clone https://github.com/sasidhar-serv/bbb-install.git
+git clone https://github.com/sasidhar-serv/bbb-install3.git
 cd bbb-install
 cd support/
 chmod +x support.sh
